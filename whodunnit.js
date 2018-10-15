8eafc4fa97d7831b4b8cbe332907b8c9ff978c55
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-15 21:12:29 +0100

    Add tweak

--- whodunnit.js
+++ whodunnit.js
@@ -55,18 +55,18 @@
 
 // Episode 4
 
-let suspectOne = 'Miss Scarlet';
-let suspectTwo = 'Professor Plum';
-let suspectThree = 'Mrs. Peacock';
-
-const declareAllSuspects = function() {
-  let suspectThree = 'Colonel Mustard';
-  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-}
-
-const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
 
 // List of suspects is returned from declareAllSuspects and const variable 'suspects'
 // is available to be displayed, so we see the list (Scarlet, Plum, Mustard).
